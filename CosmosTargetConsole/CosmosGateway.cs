b91c6101fb812c1f6654a5b27ff2cc64c9a8de62
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-09 16:22:31 -0400

    Add throughput to target file

--- CosmosTargetConsole/CosmosGateway.cs
+++ CosmosTargetConsole/CosmosGateway.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.Documents.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CosmosTargetConsole
