734de47996342b538357e46de326bc4922272008
vplauzon <vincent_lauzon@yahoo.com.au>
2018-06-11 16:04:10 -0400

    .

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+# Docker
+
+Artefacts to build a docker image available on [Docker Hub](https://hub.docker.com/r/vplauzon/cosmos-db-target-config/).
+
+The container is using a .NET Core Console App.
---
+++ docker/build-container.sh
@@ -0,0 +1,17 @@
+#	Remove temp directory
+rm temp -r
+
+#	Create temp directory
+mkdir temp
+
+#	Build console app & drop them in temp folder
+dotnet build ../CosmosTargetConsole/ -c release -o temp
+
+#	Build docker container
+sudo docker build -t vplauzon/cosmos-db-target-config .
+
+#	Publish image
+sudo docker push vplauzon/cosmos-db-target-config
+
+#	Test image
+#sudo docker run -it vplauzon/cosmos-db-target-config
