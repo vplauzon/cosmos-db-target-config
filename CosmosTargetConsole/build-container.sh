c7e450d28b749d74036fb38d991291392118d376
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-12 09:09:19 -0400

    Integrate container in ARM template

--- CosmosTargetConsole/build-container.sh
+++ CosmosTargetConsole/build-container.sh
@@ -1,12 +1,3 @@
-#	Remove temp directory
-rm temp -r
-
-#	Create temp directory
-mkdir temp
-
-#	Build console app & drop them in temp folder
-dotnet build ../CosmosTargetConsole/ -c release -o ../docker/temp
-
 #	Build docker container
 sudo docker build -t vplauzon/cosmos-db-target-config .
 
